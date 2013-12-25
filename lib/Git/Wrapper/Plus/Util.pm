88d8a4fcbdc3529fe55690ef032db2ddde38d2bc
Kent Fredric <kentfredric@gmail.com>
2013-12-26 02:25:19 +1300

    Build results of 70220fa (on master)

--- lib/Git/Wrapper/Plus/Util.pm
+++ lib/Git/Wrapper/Plus/Util.pm
@@ -22,31 +22,32 @@ use Sub::Exporter::Progressive -setup => {
 use Try::Tiny;
 use Scalar::Util qw(blessed);
 
+
 sub exit_status_handler {
   my ( $callback, $status_map ) = @_;
   my $return = 1;
-  &try(
-    $callback,
-    catch {
-      undef $return;
-      if ( not ref $_ ) {
-        die $_;
-      }
-      if ( not blessed $_ ) {
-        die $_;
-      }
-      if ( not $_->isa('Git::Wrapper::Exception') ) {
-        die $_;
-      }
-      for my $status ( sort keys %{$status_map} ) {
-        if ( $status == $_->status ) {
-          $return = $status_map->{$status}->($_);
-          return;
-        }
-      }
+  try {
+    $callback->();
+  }
+  catch {
+    undef $return;
+    if ( not ref $_ ) {
       die $_;
     }
-  );
+    if ( not blessed $_ ) {
+      die $_;
+    }
+    if ( not $_->isa('Git::Wrapper::Exception') ) {
+      die $_;
+    }
+    for my $status ( sort keys %{$status_map} ) {
+      if ( $status == $_->status ) {
+        $return = $status_map->{$status}->($_);
+        return;
+      }
+    }
+    die $_;
+  };
   return 1 if $return;
   return;
 }
@@ -66,6 +67,36 @@ Git::Wrapper::Plus::Util - Misc plumbing tools for Git::Wrapper::Plus
 =head1 VERSION
 
 version 0.001001
+
+=func C<exit_status_handler>
+
+C<Git::Wrapper> throws exceptions in a few cases, and some of these
+cases are considered normal flow control for C<Git::Wrapper::Plus>.
+
+For instance, some functions in C<git> emit no output, and return an exit code.
+
+C<Git::Wrapper> treats that circumstance as a fatal exception!.
+
+Its messy getting all the right C<try/catch> stuff going, and checking
+for the object type, and then checking if the exception type is recognised or not,
+and only then determining if the status was whitelisted.
+
+So:
+
+    use Git::Wrapper::Plus::Util qw(exit_status_handler);
+
+    my $ok = exit_status_handler ( $code , {
+        1 => sub { undef }
+    });
+
+The above code normally executes C<$code>, and returns C<1> if no exception occurred.
+
+If an exception occurred, and it is not a C<Git::Wrapper::Exception>, it is simply rethrown.
+
+And for any status codes listed in the map, the attached C<sub> is executed, and C<exit_status_handler>
+propagates its return value.
+
+Any other circumstances ( like a status code not existing in the map ) are simply rethrown.
 
 =head1 AUTHOR
 
