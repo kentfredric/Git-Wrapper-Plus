b223776557f0238d901b2932054625ff0d238dbe
Kent Fredric <kentfredric@gmail.com>
2014-01-13 08:49:18 +1300

    Refs.pm: Use for-each-ref where supported instead of ls_remote

--- lib/Git/Wrapper/Plus/Refs.pm
+++ lib/Git/Wrapper/Plus/Refs.pm
@@ -25,7 +25,14 @@ B<REQUIRED>: A Git::Wrapper compatible object
 
 =cut
 
-has git => required => 1, is => ro =>;
+has 'git' => ( required => 1, is => ro => );
+has 'support' => ( is => ro =>, lazy => 1, builder => 1 );
+
+sub _build_support {
+  my ( $self, ) = @_;
+  require Git::Wrapper::Plus::Support;
+  return Git::Wrapper::Plus::Support->new( git => $self->git );
+}
 
 =head1 SYNOPSIS
 
@@ -54,6 +61,28 @@ sub _for_each_ref {
   my ( $self, $refspec, $callback ) = @_;
 
   my $git_dir = $self->git->dir;
+
+  # git for-each-ref refs/heads/** ==
+  #     for-each-ref refs/heads/*  ==
+  #     ls-remote    refs/heads/*  + exclude refs/heads/*/*
+  #
+  # git for-each-refs refs/heads/ ==
+  #     ls-remote     refs/heads/*
+  #
+  if ( $self->support->supports_command('for-each-ref') ) {
+    if ( $refspec =~ qr{\A(.*)/[*]{1,2}\z}msx ) {
+      $refspec = $1;
+    }
+    for my $line ( $self->git->for_each_ref($refspec) ) {
+      if ( $line =~ qr{ \A ([^ ]+) [^\t]+ \t ( .+ ) \z }msx ) {
+        $callback->( $1, $2 );
+        next;
+      }
+      require Carp;
+      Carp::confess( 'Regexp failed to parse a line from `git for-each-ref` :' . $line );
+    }
+    return;
+  }
   for my $line ( $self->git->ls_remote( $git_dir, $refspec ) ) {
     ## no critic (Compatibility::PerlMinimumVersionAndWhy)
     if ( $line =~ qr{ \A ([^\t]+) \t ( .+ ) \z }msx ) {
