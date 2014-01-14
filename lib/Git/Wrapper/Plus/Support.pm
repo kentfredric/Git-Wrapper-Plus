8f446f813e822b7750c7408da770bfbfc2e0a73a
Kent Fredric <kentfredric@gmail.com>
2014-01-15 07:48:37 +1300

    Support.pm: Factor out commands logic

--- lib/Git/Wrapper/Plus/Support.pm
+++ lib/Git/Wrapper/Plus/Support.pm
@@ -37,67 +37,13 @@ sub _build_versions {
   return Git::Wrapper::Plus::Versions->new( git => $self->git );
 }
 
-## no critic (ProhibitPackageVars)
+has 'commands' => ( is => ro =>, lazy => 1, builder => 1 );
 
-our $command_db = {
-  'for-each-ref' => [
-    {
-      'min'      => '1.4.4',
-      'min_tag'  => '1.4.4-rc1',
-      'min_sha1' => '9f613ddd21cbd05bfc139d9b1551b5780aa171f6',
-    },
-  ],
-  'init' => [
-    {
-      'min'      => '1.5.0',
-      'min_tag'  => '1.5.0-rc1',
-      'min_sha1' => '515377ea9ec6192f82a2fa5c5b5b7651d9d6cf6c',
-    },
-  ],
-  'update-cache' => [
-    {
-      'min'      => '0.99',
-      'min_tag'  => '0.99',
-      'min_sha1' => 'e83c5163316f89bfbde7d9ab23ca2e25604af290',
-      'max'      => '1.0.0',
-      'max_tag'  => '1.0.0',
-      'max_sha1' => 'ba922ccee7565c949b4db318e5c27997cbdbfdba',
-    },
-  ],
-  'update-index' => [
-    {
-      'min'      => '0.99.7',
-      'min_tag'  => '0.99.7',
-      'min_sha1' => '215a7ad1ef790467a4cd3f0dcffbd6e5f04c38f7',
-    },
-  ],
-  'ls-remote' => [
-    {
-      'min'      => '0.99.2',
-      'min_tag'  => '0.99.2',
-      'min_sha1' => '0fec0822721cc18d6a62ab78da1ebf87914d4921',
-    },
-  ],
-  'peek-remote' => [
-    {
-      'min'      => '0.99.2',
-      'min_tag'  => '0.99.2',
-      'min_sha1' => '18705953af75aed190badfccdc107ad0c2f36c93',
-    },
-  ],
-};
-
-my (@GIT_ZERO_LIST) = qw( init-db cat-file show-diff write-tree read-tree commit-tree );
-
-for my $cmd (@GIT_ZERO_LIST) {
-  $command_db->{$cmd} = [
-    {
-      'min'      => '0.99',
-      'min_tag'  => '0.99',
-      'min_sha1' => 'e83c5163316f89bfbde7d9ab23ca2e25604af290',
-    },
-  ];
+sub _build_commands {
+  require Git::Wrapper::Plus::Support::Commands;
+  return Git::Wrapper::Plus::Support::Commands->new();
 }
+
 
 =method C<supports_command>
 
@@ -123,30 +69,8 @@ B<Currently indexed commands>
 
 sub supports_command {
   my ( $self, $command ) = @_;
-  if ( not exists $command_db->{$command} ) {
-    return;
-  }
-  for my $pair ( @{ $command_db->{$command} } ) {
-    if ( exists $pair->{min} and not exists $pair->{max} ) {
-      if ( $self->versions->newer_than( $pair->{min} ) ) {
-        return 1;
-      }
-      return 0;
-    }
-    if ( exists $pair->{max} and not exists $pair->{min} ) {
-      if ( $self->versions->older_than( $pair->{max} ) ) {
-        return 1;
-      }
-      return 0;
-    }
-    if ( not exists $pair->{max} and not exists $pair->{min} ) {
-      warn 'Bad quality command db entry with no range control';
-      next;
-    }
-    next unless $self->versions->newer_than( $pair->{min} );
-    next unless $self->versions->older_than( $pair->{max} );
-    return 1;
-  }
+  return unless $self->commands->has_entry($command);
+  return 1 if $self->commands->entry_supports( $command, $self->versions );
   return 0;
 }
 
